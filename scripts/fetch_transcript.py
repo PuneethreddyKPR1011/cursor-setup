fcc10c93a6525f937d819837c1e9492fe0f2392c
Kamalapuram Puneeth <puneethkamalapuram@gmail.com>
2026-04-01 10:36:08 +0530

    updated

--- scripts/fetch_transcript.py
+++
@@ -1,8 +0,0 @@
-from youtube_transcript_api import YouTubeTranscriptApi
-
-video_id = "hyYCn_kAngI"
-
-transcript = YouTubeTranscriptApi().fetch(video_id)
-
-for entry in transcript:
-    print(entry.text)
